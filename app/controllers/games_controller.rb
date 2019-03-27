88e8b8ea8fa4cb9400c60e59aa53d9c0d179909c
Macbook <macbook@netrestore-a4d18cc5150e.local>
2019-03-27 19:17:14 +0100

    message

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,25 @@
 class GamesController < ApplicationController
+ QUESTIONS = [
+    "A t'il les yeux clairs ?",
+    "Est ce homme ?",
+    "Est il vegan ?",
+    "Est il originaire d'aquitaine ?"
+]
+
+
+
   def create
   	# créer le tableau de réponses vide
-  	redirect_to edit_game_path(1), method: :get 
+  	@game = Game.create
+  	redirect_to edit_game_path(@game["id"], question_id: 0), method: :get 
   end
   def edit
-
+    #@id = params["id"]
+    @question = QUESTIONS[params["question_id"].to_i]
   end
 
   def update
-  	redirect_to game_path(1), method: :get
+  	
   end
 
   def show
